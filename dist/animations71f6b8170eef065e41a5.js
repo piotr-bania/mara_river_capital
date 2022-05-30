362ca6bd49fdc77211d2c4a67c2a1bec65ac9ffd
Piotr Bania <piotr.bania@europe.com>
2022-05-30 16:01:01 +0100

    init

--- dist/animations71f6b8170eef065e41a5.js
+++ dist/animations71f6b8170eef065e41a5.js
@@ -6428,28 +6428,28 @@ tl.to(square, 2.5, {
   opacity: 0.7,
   ease: gsap__WEBPACK_IMPORTED_MODULE_2__.Power2.easeOut
 }, "-=2.25");
-tl.to(logoSml, 2, {
+tl.to(logoSml, 2.25, {
   opacity: 1,
   scale: 1,
   ease: gsap__WEBPACK_IMPORTED_MODULE_2__.Power2.easeOut
-}, "-=1.5");
+}, "-=1.25");
 tl.fromTo(header, {
   opacity: 0,
-  y: 200,
+  y: 250,
   ease: gsap__WEBPACK_IMPORTED_MODULE_2__.Power2.easeOut
 }, {
   opacity: 1,
   y: 0,
   duration: 1.5
-}, "-=1.75");
+}, "-=1.5");
 tl.fromTo(paragraph, {
   opacity: 0,
-  y: 500,
+  y: 800,
   ease: gsap__WEBPACK_IMPORTED_MODULE_2__.Power2.easeOut
 }, {
   opacity: 1,
   y: 0,
-  duration: 1.5
+  duration: 2
 }, "-=1.75");
 btnAbout.addEventListener('click', function () {
   tl.play();
@@ -6458,4 +6458,4 @@ btnAbout.addEventListener('click', function () {
 
 /******/ })()
 ;
-//# sourceMappingURL=animationscc56a124dc35097f67d1.js.map
+//# sourceMappingURL=animations71f6b8170eef065e41a5.js.map
--- src/animations.js
+++ src/animations.js
@@ -41,30 +41,30 @@ tl.to(square, 2.5, {
     ease: Power2.easeOut
 }, "-=2.25")
 
-tl.to(logoSml, 2, {
+tl.to(logoSml, 2.25, {
     opacity: 1,
     scale: 1,
     ease: Power2.easeOut,
-}, "-=1.5")
+}, "-=1.25")
 
 tl.fromTo(header, {
     opacity: 0,
-    y: 200,
+    y: 250,
     ease: Power2.easeOut
 },  {
     opacity: 1,
     y: 0,
     duration: 1.5
-}, "-=1.75")
+}, "-=1.5")
 
 tl.fromTo(paragraph, {
     opacity: 0,
-    y: 500,
+    y: 800,
     ease: Power2.easeOut,
 }, {
     opacity: 1,
     y: 0,
-    duration: 1.5
+    duration: 2
 }, "-=1.75")
 
 btnAbout.addEventListener('click', () => {
