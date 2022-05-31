1cf86ce4732f1aa771983b93f49acb427193048e
Piotr Bania <piotr.bania@europe.com>
2022-06-01 00:32:02 +0100

    init

--- src/animations.js
+++ src/animations.js
@@ -10,11 +10,6 @@ import {
     Sine
 } from "gsap"
 
-const sizes = {
-    width: window.innerWidth,
-    height: window.innerHeight
-}
-
 const btnHome = document.querySelector('.btnHome')
 const btnAbout = document.querySelector('.btnAbout')
 const romboid = document.querySelector('.coverRomboidImg')
@@ -25,42 +20,42 @@ const header = document.getElementById('header1')
 const paragraph = document.getElementById('paragraph1')
 const maraImg = document.getElementById('backgroundImg')
 
-const tl = gsap.timeline({
+const tlAbout = gsap.timeline({
     paused: true,
     reversed: true
 })
 
-tl.to(romboid, 2.5, {
+tlAbout.to(romboid, 2.5, {
     scale: 5,
     opacity: 0,
     ease: Power2.easeOut
 })
 
-tl.to(logoLrg, 2.5, {
+tlAbout.to(logoLrg, 2.5, {
     scale: 0.8,
     opacity: 0,
-    ease: Power2.easeOut,
+    ease: Power2.easeOut
 }, "-=2.5")
 
-tl.to(maraImg, 2.5, {
+tlAbout.to(maraImg, 2.5, {
     scale: 1.6,
     x: -25,
     y: 210,
-    ease: Power2.easeOut,
+    ease: Power2.easeOut
 }, "-=2.5")
 
-tl.to(square, 2.5, {
+tlAbout.to(square, 2.5, {
     opacity: 0.62,
     ease: Power2.easeOut
 }, "-=2.5")
 
-tl.to(logoSml, 2.5, {
+tlAbout.to(logoSml, 2.5, {
     opacity: 1,
     scale: 1,
-    ease: Power2.easeOut,
+    ease: Power2.easeOut
 }, "-=1.75")
 
-tl.fromTo(header, {
+tlAbout.fromTo(header, {
     opacity: 0,
     y: 250,
     ease: Power2.easeOut
@@ -70,10 +65,10 @@ tl.fromTo(header, {
     duration: 2
 }, "-=2")
 
-tl.fromTo(paragraph, {
+tlAbout.fromTo(paragraph, {
     opacity: 0,
     y: 800,
-    ease: Power2.easeOut,
+    ease: Power2.easeOut
 }, {
     opacity: 1,
     y: 0,
@@ -81,29 +76,29 @@ tl.fromTo(paragraph, {
 }, "-=2.25")
 
 btnAbout.addEventListener('click', () => {
-    tl.play()
+    tlAbout.play()
 })
 
 btnHome.addEventListener('click', (e) => {
-    if (tl.isActive()) {
+    if (tlAbout.isActive()) {
         e.preventDefault()
         e.stopImmediatePropagation()
         return false
     }
-    tl.reverse()
+    tlAbout.reverse()
 })
 
 logoSml.addEventListener('click', () => {
-    tl.play()
+    tlAbout.play()
 })
 
 logoSml.addEventListener('click', (e) => {
-    if (tl.isActive()) {
+    if (tlAbout.isActive()) {
         e.preventDefault()
         e.stopImmediatePropagation()
         return false
     }
-    tl.reverse()
+    tlAbout.reverse()
 })
 
 function toggleTween(tween) {
